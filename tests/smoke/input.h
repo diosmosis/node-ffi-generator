7fda2cbc8a5dd3719936d4b5697f090f7fa69f26
diosmosis <benaka@piwik.pro>
2014-11-14 01:58:18 -0800

    Adding more code to smoke test.

--- tests/smoke/input.h
+++ tests/smoke/input.h
@@ -2,9 +2,10 @@
 
 /*
  * things to check:
- *    include directories
- *    function
- *    enum
+ *    stdint typedefs            (YES!)
+ *    basic function             (YES!)
+ *    normal int types
+ *    enum                       (YES!)
  *    parameter types
  *    pointer types
  *    string types
@@ -15,6 +16,19 @@
  *    anonymous struct
  *    anonymous union
  *    function pointer?
+ *    const/volatile are removed
  */
 
-int16_t my_function();
+enum my_enum {
+    VAR_ONE = -1,
+
+    VAR_TWO = 23,
+
+    VAR_THREE,
+
+    VAR_FOUR,
+
+    five =555
+};
+
+int16_t my_function(my_enum param);
